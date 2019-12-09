1629db03aff9edf42832da8afd6f525ba63d5026
Rylene Rika <rrika@mi-p2.21-school.ru>
2019-12-09 17:01:39 +0300

    start

--- corewar_source/includes/corewar.h
+++ corewar_source/includes/corewar.h
@@ -6,7 +6,7 @@
 /*   By: rrika <rrika@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 15:56:31 by swarner           #+#    #+#             */
-/*   Updated: 2019/12/06 19:13:17 by rrika            ###   ########.fr       */
+/*   Updated: 2019/12/09 16:19:23 by rrika            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ typedef struct		s_champ
 	int id; //уникальный идентификационный номер
 	char *name;
 	char *comment;
-	unsigned char *size; //размер исполняемого кода
+	int	size; //размер исполняемого кода
 	unsigned char *code; //исполныемый код
 }									t_champ;
 
--- corewar_source/src/main.c
+++ corewar_source/src/main.c
@@ -6,7 +6,7 @@
 /*   By: rrika <rrika@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/13 15:44:25 by swarner           #+#    #+#             */
-/*   Updated: 2019/12/06 19:15:54 by rrika            ###   ########.fr       */
+/*   Updated: 2019/12/09 17:00:27 by rrika            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,11 +19,22 @@
 #include <unistd.h>
 #include <string.h>
 
+void	error(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	write(1, str, i);
+	exit(1);
+}
+
 t_champ	*init(int id)
 {
 	t_champ *player;
 
-	if (!(player = (t_champ *)ft_memalloc(sizeof(t_champ))))
+	if (!(player = (t_champ *)malloc(sizeof(t_champ))))
 		error("error with malloc");
 	player->id = id;
 	player->name = NULL;
@@ -38,67 +49,104 @@ void	check_header(int fd)
 	unsigned int i;
 	unsigned char buff[5];
 
-	if ((read(fd, buff, 4) < 0)
-	    error("error in reading file");
+	if ((read(fd, buff, 4) < 0))
+		error("error in reading file");
 	i = (buff[0] << 24) | (buff[1] << 16) | (buff[2] << 8) | (buff[3]);
 	if (i != COREWAR_EXEC_MAGIC)
 		error("wrong magic header");
 }
 
-char	*miss_nulls(int fd)
+int		miss_nulls(int fd)
 {
-    int	size;
-    unsigned char buff[5];
+	int	size;
+	int i;
+	unsigned char buff[5];
 
-    if ((read(fd, buff, 4) < 0)
-        error("error in reading file");
-    return ((char*)buff);
+	if ((size = read(fd, &buff, 4) < 0))
+		error("error in reading file");
+	if (size < 4)
+		error("wrong size");
+	i = (buff[0] << 24) | (buff[1] << 16) | (buff[2] << 8) | (buff[3]);
+	return (i);
 }
 
 char	*check_name(int fd)
 {
 	int size;
-	unsigned char buff[PROG_NAME_LENGTH + 1];
+	char *buff;
 
-	if ((size = read(fd, buff, PROG_NAME_LENGTH) < 0)
-        error("error in reading file");
+	if (!(buff = malloc(PROG_NAME_LENGTH + 1)))
+		error("error in malloc");
+	if ((size = read(fd, &buff, PROG_NAME_LENGTH) < 0))
+		error("error in reading file");
 	if (size < PROG_NAME_LENGTH)
-	    error("bad name");
-	return ((char*)buff);
-
+		error("bad name");
+	return (buff);
 }
 
-unsigned char *size_of_code(int fd)
+char	*check_comment(int fd)
 {
-    int size;
-    unsigned char buff[5];
+	int	size;
+	char *buff;
 
-    if ((read(fd, buff, 4) < 0)
-        error("error in reading file");
-    if (size < 4)
-        error("wrong size of code");
-    return (buff);
+	if (!(buff = malloc(COMMENT_LENGTH + 1)))
+		error("error in malloc");
+	if ((size = read(fd, &buff, COMMENT_LENGTH) < 0))
+		error("error in reading file");
+	if (size < COMMENT_LENGTH)
+		error("wrong comment");
+	return (buff);
+}
+
+int	size_of_code(int fd)
+{
+	int i;
+	int size;
+	unsigned char buff[5];
+
+	if ((size = read(fd, &buff, 4) < 0))
+		error("error in reading file");
+	if (size < 4)
+		error("wrong size of code");
+	i = (buff[0] << 24) | (buff[1] << 16) | (buff[2] << 8) | (buff[3]);
+	return (i);
+}
+
+unsigned char	*check_code(int fd, int len)
+{
+	int size;
+	unsigned char *buff;
+
+	if (!(buff = malloc(len + 1)))
+		error("error in malloc");
+	if ((size = read(fd, &buff, len) < 0))
+		error("error in reading file");
+	if (size < len)
+		error("wrong code");
+	return (buff);
 }
 
 int     main(int argc, char **argv)
 {
 	int fd;
 	int id;
-	unsigned char buff[1000];
 	t_champ *player;
 
 	id = 1;
 	player = init(id);
-	if ((fd = open(argv[1], O_RDONLY) < 0)
-	    error("error in opening file");
+	if ((fd = open(argv[1], O_RDONLY) < 0))
+		error("error in opening file");
 	check_header(fd);
 	player->name = check_name(fd);
-	miss_nulls(fd);
-	if ((player->size = size_of_code(fd)) > CHAMP_MAX_SIZE)
-	    error("code is too big");
+	if (miss_nulls(fd) != 0)
+		error("no null");
+	if ((player->size = size_of_code(fd)) > CHAMP_MAX_SIZE || player->size < 0)
+		error("code is too big");
+	player->comment = check_comment(fd);
+	if (miss_nulls(fd) != 0)
+		error("no null");
+	player->code = check_code(fd, player->size);
 
-
-	
 	/* printf("%x ", buff[0]);
 	printf("%x ", buff[1]);
 	printf("%x ", buff[2]);
