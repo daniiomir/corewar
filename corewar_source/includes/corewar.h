9c66da6bb61ad532b08a07ca824ae23f6bceb5e6
Clark Nikia <cnikia@mi-p4.21-school.ru>
2020-01-10 14:20:16 +0300

    error libft.h done

--- corewar_source/includes/corewar.h
+++ corewar_source/includes/corewar.h
@@ -4,7 +4,7 @@
 # include <fcntl.h>
 # include <stdio.h>
 # include <stdlib.h>
-# include "libft.h"
+# include "../lib/includes/libft.h"
 # include "op.h"
 # include "structures.h"
 # include "macroses.h"
